2e0b4601e945212ab2784f66e130a4291f16f9ad
xXCoffeeColaXc <tamsyandro@gmail.com>
2023-10-26 00:12:12 +0200

    save and load checkpoint added

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import torch
 import numpy as np
+import config
+from torchvision.utils import save_image
 
+def denorm():
+    pass
 
 def label2onehot(labels, dim):
     """Convert label indices to one-hot vectors."""
@@ -12,3 +16,60 @@ def label2onehot(labels, dim):
 def onehot2label(labels):
     """Convert one-hot vectors to label indices """
     return np.where(labels.numpy() == 1)[1]
+
+
+# THIS IS SHIT
+def save_some_examples(gen, val_loader, epoch, folder):
+    x, y = next(iter(val_loader))
+    x, y = x.to(config.DEVICE), y.to(config.DEVICE)
+
+    rand_idx = torch.randperm(y.size(0)) # Generate target domain labels randomly.
+    label_trg = y[rand_idx]
+
+    label_trg = label_trg.to(config.DEVICE)
+
+    conditions = ['fog', 'night', 'rain', 'snow', 'daytime']
+
+    gen.eval()
+    with torch.no_grad():
+        y_fake = gen(x, label_trg)
+        y_fake = y_fake * 0.5 + 0.5  # remove normalization
+        save_image(y_fake, folder + f"/y_gen_{epoch}_{conditions[onehot2label(label_trg)]}.png")
+        save_image(x * 0.5 + 0.5, folder + f"/input_{epoch}.png")
+    gen.train()
+
+def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    checkpoint = {
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+    }
+    torch.save(checkpoint, filename)
+
+def load_checkpoint(checkpoint_file, model, optimizer, lr):
+    print("=> Loading checkpoint")
+    checkpoint = torch.load(checkpoint_file, map_location=config.DEVICE)
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+
+    # If we don't do this then it will just have learning rate of old checkpoint
+    # and it will lead to many hours of debugging \:
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+def build_model():
+    pass
+
+def print_model():
+    pass
+
+def restore_model():
+    pass
+
+def save_model():
+    pass
+
+def build_tensorboard():
+    pass
+
+
