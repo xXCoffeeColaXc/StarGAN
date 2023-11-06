2020ee3f49dfb80b601423b3dad83ab6f7ed0090
xXCoffeeColaXc <tamsyandro@gmail.com>
2023-11-06 21:06:33 +0100

    regroup dirs, selected_domains

--- config.py
+++ config.py
@@ -5,18 +5,20 @@ DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 # Directories
 DAWN_DATASET_DIR = "/media/talmacsi/48a93eb4-f27d-48ec-9f74-64e475c3b6ff/Downloads/766ygrbt8y-3/DAWN"
 ACDC_DATASET_DIR = "/media/talmacsi/48a93eb4-f27d-48ec-9f74-64e475c3b6ff/Downloads/rgb_anon_trainvaltest/rgb_anon"
+# ACDC_DATASET_DIR = "/data/rgb_anon"
+DATA_DIR = 'data'
 LOG_DIR = "logs/"
-RESULT_DIR = "results/"
-EVAL_DIR = "eval/"
-SAVED_MODELS_DIR = "saved_models/"
-
+RUN_NAME = "BASELINE_2" # 2 condition
+OUTPUT_IMG_DIR = "output/images/" + RUN_NAME
+OUTPUT_MODELS_DIR = "output/saved_models/" + RUN_NAME
 CHECKPOINT_DISC = "disc.pth.tar"
 CHECKPOINT_GEN = "gen.pth.tar"
 
 # Model config
 IMAGE_SIZE = 128
 CHANNEL_IMG = 3
-NUM_DOMAINS = 5
+SELECTED_DOMAIN = ['daytime', 'fog'] # ['fog', 'night', 'rain', 'snow', 'daytime']
+NUM_DOMAINS = len(SELECTED_DOMAIN)
 LAMBDA_CLS = 1
 LAMBDA_REC = 10
 LAMBDA_GP = 10
@@ -30,7 +32,7 @@ NUM_EPOCHS = 200
 N_CRITIC = 5 # number of D updates per each G update 
 NUM_WORKERS = 4
 SAVE_MODEL = True
-LOAD_MODEL = True
+LOAD_MODEL = False
 LOG_STEP = 10
 LOG = True
 
